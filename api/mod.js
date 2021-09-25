eb369532388f41e10f43bcda80e4ce36107ecc20
Clément <le.mikmac@gmail.com>
2021-09-25 04:57:07 +0200

    Improve error messaging and fix cursor null error

--- api/mod.js
+++ api/mod.js
@@ -5,22 +5,25 @@ const headers = {
 }
 
 const API = async (path, data) => {
-	const res = await fetch(`https://api.notion.com/v1/${path}`, {
-		body: JSON.stringify(data),
-		headers,
-		method: "POST",
-	})
+	const params = { method: "POST", headers, body: JSON.stringify(data) }
+	const res = await fetch(`https://api.notion.com/v1/${path}`, params)
 
 	if (res.status < 200 || res.status > 299) {
 		const body = await res.text()
-		console.log(path, data, body)
-		throw Error(`API Error: ${res.status} - ${res.statusText}`)
+		try {
+			const { code, message } = JSON.parse(body)
+			console.log(path, data, { status: res.status, code, message })
+			throw Error(`API Error: ${code} - ${message}`)
+		} catch {
+			console.log(path, data, body)
+			throw Error(`API Error: ${res.status} - ${res.statusText}`)
+		}
 	}
 
 	return res.json()
 }
 
-const fetchDatabase = async (databaseId, cursor = null) => {
+const fetchDatabase = async (databaseId, cursor) => {
 	const params = { start_cursor: cursor, page_size: 100 }
 	const response = await API(`databases/${databaseId}/query`, params)
 	if (!response.next_cursor) return response.results
